c3178f354b730704f639d481f6c2deb7d5ddcf49
Ian Kamau <kian99564@gmail.com>
2023-10-04 09:36:57 +0300

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Person from "./components/Person";
+import School from "./components/School";
 
 function App() {
-  const schools = [
-    { name: "Everest", location: "Nairobi" },
-    { name: "Alliance", location: "Kiambu" },
-  ];
   return (
     <div>
-      <Navbar />
-
-      {schools.map((school, index) => {
-        return (
-          <Person key={index} school={school.name} location={school.location} />
-        );
-      })}
-
-      {/* <img
-        src="https://images.unsplash.com/photo-1668383184143-3896b060259a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDR8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        alt="nature.png"
-      /> */}
+      <School />
     </div>
   );
 }
---
+++ src/components/School.jsx
@@ -0,0 +1,12 @@
+function School() {
+  return (
+    <div>
+      <h1>Everest School</h1>
+      <h1>Signin</h1>
+      <input placeholder="Enter your name" />
+    </div>
+  );
+}
+
+
+export default School;
