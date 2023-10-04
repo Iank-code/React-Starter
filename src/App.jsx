4efa18358a81760ed31f74ebbd527ba3ee1806e6
Ian Kamau <kian99564@gmail.com>
2023-10-04 11:41:51 +0300

    React Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import School from "./components/School";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+
+  // Function that increases the number
+  // function addNumber() {
+  //   return setNumber(number + 1);
+  // }
+
+  // Function that decreases the number
+  // function decreaseNumber() {
+  //   return setNumber(number - 1);
+  // }
+
+  // useEffect(() => {
+  //   console.log("Number has increased by one");
+  // }, [number]);
+  function submitForm(event) {
+    event.preventDefault();
+    alert(`Hello ${name} your password is ${password}`);
+  }
   return (
     <div>
-      <School />
+      {/* <h1>{number}</h1>
+      <button onClick={addNumber}>Add</button>
+      <br />
+      <button onClick={decreaseNumber}>Decrease</button> */}
+      {/* <School /> */}
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          onChange={(event) => setName(event.target.value)}
+          required
+        />
+
+        {/* Input for password */}
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(event) => setPassword(event.target.value)}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
